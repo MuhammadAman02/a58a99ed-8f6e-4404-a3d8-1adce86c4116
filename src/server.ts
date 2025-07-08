304fdc42626141f30c34ca519c0425fc99dedf32
MuhammadAman02 <mohammada@hst.ie>
2025-07-08 16:04:35 +0500

    update file

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,11 @@
-import Fastify from 'fastify';
 import app from './app';
 
 const start = async () => {
-  const server = Fastify({ logger: true });
-
   try {
-    await server.register(app);
-
-    await server.listen({ port: 3000, host: '0.0.0.0' }); // important for K8s
-    console.log('Server listening on http://0.0.0.0:3000');
+    await app.listen({ port: 3000, host: '0.0.0.0' });
+    console.log('Server running on http://localhost:3000');
   } catch (err) {
-    server.log.error(err);
+    app.log.error(err);
     process.exit(1);
   }
 };
